04c2864de6fb3401248bf6a07273920120abc547
Michael Aviani <47023272+avianillc001@users.noreply.github.com>
2019-04-17 20:43:04 -0700

    Initial upload of zoho salesiq javascript
    Zoho SalesIQ javascript for tracking and Live Chat integration.
    
    **Must be placed BEFORE the </body> tag

---
+++ zoho_salesiq.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || 
+{widgetcode:"fc5cc97c2d4cd26442ead65d1d9acf318e7948894c15f44ede35919629a73c2019c6c48416521ecae763ad0896f9bf7a", values:{},ready:function(){}};
+var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;
+s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+</script>
